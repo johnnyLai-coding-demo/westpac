cea2dcdb36bc618ac7f9b1e9ddb60fbc149973ad
Johnny Lai <huging@gmail.com>
2021-04-29 05:28:25 +1200

    new method to request post info from endpoint

--- src/main/java/com/fifi/java/practise/springhibernate/PostReadingApplication.java
+++ src/main/java/com/fifi/java/practise/springhibernate/PostReadingApplication.java
@@ -1,6 +1,13 @@
 package com.fifi.java.practise.springhibernate;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -9,7 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +26,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.fifi.java.practise.springhibernate.obj.Post;
+import com.fifi.java.practise.springhibernate.obj.PostLoaded;
 import com.fifi.java.practise.springhibernate.repository.PostRepository;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+
 
 @SpringBootApplication
 @RestController
@@ -64,13 +79,48 @@ public class PostReadingApplication implements CommandLineRunner {
         return new ResponseEntity<Object>(posts, HttpStatus.OK);
 	}	
 	
+	
+	public static void requestPost() throws IOException, JSONException {
+		
+		Gson gson = new Gson();				
+	    URL urlForGetRequest = new URL("https://jsonplaceholder.typicode.com/posts/");
+	    String readLine = null;
+	    String result = "";
+	    HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+	    conection.setRequestMethod("GET");
+
+	    int responseCode = conection.getResponseCode();
+
+
+	    if (responseCode == HttpURLConnection.HTTP_OK) {
+	        BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()));
+
+	        while ((readLine = in .readLine()) != null) {
+
+	            result = result + readLine;
+
+	        } 
+	        in.close();	    	   
+	        
+	        Type listType = new TypeToken<List<PostLoaded>>(){}.getType();
+	        List<PostLoaded> posts = gson.fromJson(result, listType);
+
+	        PostLoaded postLoaded = posts.get(0);
+	        System.out.println(postLoaded);
+
+	    } else {
+	        System.out.println("NOT WORKING");
+	    }
+
+	}
+	
 	@Override		
 	public void run(String... args) throws Exception {
 
 //		logger.info("Student id 10001 -> {}", repository.findById(1L));
 //		
-		logger.info("All posts -> {}", repository.findAll());
-		
+		//logger.info("All posts -> {}", repository.findAll());
+		requestPost();
 		//Insert
 //		logger.info("Inserting -> {}", repository.save(new Post("John", 1L)));
 //		logger.info("Inserting -> {}", repository.save(new Post("John2", 1L)));
