35ccc3efe844301c9f1854c2d10b66ae4d13f250
Johnny Lai <huging@gmail.com>
2021-05-01 20:58:44 +1200

    remove unused import

--- src/main/java/com/fifi/java/practise/springhibernate/PostReadingApplicationAPI.java
+++ src/main/java/com/fifi/java/practise/springhibernate/PostReadingApplicationAPI.java
@@ -2,11 +2,10 @@ package com.fifi.java.practise.springhibernate;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +15,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fifi.java.practise.springhibernate.obj.Comment;
-import com.fifi.java.practise.springhibernate.obj.Post;
-import com.fifi.java.practise.springhibernate.repository.CommentDBfactory;
+
 import com.fifi.java.practise.springhibernate.repository.CommentRepository;
-import com.fifi.java.practise.springhibernate.repository.PostDBfactory;
+
 import com.fifi.java.practise.springhibernate.repository.PostRepository;
 
 @RestController
@@ -54,8 +51,6 @@ public class PostReadingApplicationAPI
 	@ResponseBody
 	public ResponseEntity<Object> getComments(@RequestParam(value = "postId") String postId) {
 						
-		//System.out.println(postId);
-		//List comments = repository.findCommentByPostId(Long.parseLong(postId));
 		List comments = commentRepository.findCommentByPostId(Long.parseLong(postId));
 		
         return new ResponseEntity<Object>(comments, HttpStatus.OK);
@@ -64,9 +59,6 @@ public class PostReadingApplicationAPI
 	//path parameter
 	@GetMapping(path = "/posts", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> page() {
-		//@RequestParam(value = "startDate") String startDate, @RequestParam(value = "endDate") String endDate
-		//List posts = repository.findPostByDate(java.sql.Timestamp.valueOf(startDate + " 00:00:00"), java.sql.Timestamp.valueOf(endDate + " 23:59:59"));
-
 		List posts = postRepository.findAll();
 		
         return new ResponseEntity<Object>(posts, HttpStatus.OK);
